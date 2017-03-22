228a3f97a7b12def8293545f83c8d1607af75d29
Italo Paiva <italosensei@hotmail.com>
2017-03-22 19:01:09 -0300

    Adding timestamp fields in models and unique constraint in UserGroup

--- chat/models.py
+++ chat/models.py
@@ -5,12 +5,20 @@ class Group(models.Model):
     visibility = models.BooleanField(default=True)
     name = models.CharField(max_length=50)
     members = models.ManyToManyField(User, through='UserGroup')
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 class UserGroup(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     date_joined = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ('user', 'group')
+
 class Post(models.Model):
     group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
     content = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
