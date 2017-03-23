95feed3262e5019358708c46d5991459b8566ce5
Italo Paiva <italosensei@hotmail.com>
2017-03-23 00:12:52 -0300

    Refactoring models to save posts into a Group

--- chat/models.py
+++ chat/models.py
@@ -19,6 +19,7 @@ class UserGroup(models.Model):
         unique_together = ('user', 'group')
 
 class Post(models.Model):
-    group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
